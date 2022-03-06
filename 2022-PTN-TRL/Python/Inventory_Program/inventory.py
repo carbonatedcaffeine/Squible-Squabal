a50708966a34f211a76e8aa6a3c489dade7fa30d
Camden Bruce <camden.o.b@gmail.com>
2022-03-07 09:56:55 +1300

    Update file

--- 2022-PTN-TRL/Python/Inventory_Program/inventory.py
+++ 2022-PTN-TRL/Python/Inventory_Program/inventory.py
@@ -1,5 +1,4 @@
 # Camden Bruce
-from cgitb import text
 from tkinter import *
 
 def main(): # Run the program
@@ -10,12 +9,12 @@ def main(): # Run the program
 
 def pack_window(): # Fills the window with buttons, labels and text entries
 #   Label(main_window, text="LOLOLOLOL") .grid(column=1,row=0)  #Deprecated test string
-    Button(main_window, text="Quit",command=main_window.destroy, width=8) .place(x=0, y=0)
-    Button(main_window, text="Append list", width=14, height=6) .place(x=345,y=0)
+    Button(main_window, text="Quit",command=main_window.destroy, width=8, bg='#FF605C', fg='white') .place(x=0, y=0)
+    Button(main_window, text="Append list", width=8, height=6, bg='#FFBD44', fg='white') .place(x=0,y=25)
     Label(main_window, text="Name") .place(x=170,y=0)
     Label(main_window, text="Receipt number") .place(x=118,y=25)
     Label(main_window, text="Item code") .place(x=148,y=50)
-    Label(main_window, text="Quantity of item")# .place(x=,y=75)
+    Label(main_window, text="Quantity of item").place(x=115,y=75)
     entry_client_name = Entry(main_window)
     entry_client_name.place(x=220,y=0)
     entry_receipt_number = Entry(main_window)
@@ -27,5 +26,6 @@ def pack_window(): # Fills the window with buttons, labels and text entries
 
 main_window =Tk()
 main_window.title('Stock inventory')
-main_window.geometry('450x100')
+main_window.geometry('450x200')
 main()
+
