7cd080809dacd1c2b9945fe48c8d909851e75a32
Camden Bruce <camden.o.b@gmail.com>
2022-03-14 14:28:21 +1300

    Update file

--- 2022-PTN-TRL/Python/Inventory_Program/inventory.py
+++ 2022-PTN-TRL/Python/Inventory_Program/inventory.py
@@ -10,9 +10,12 @@ def main(): # Run the program
 def append_list():
     if len(entry_client_name.get())!= 0 :
         j_names.append(entry_client_name.get())
-        number['total_entries'] += 1
+        number['total_names'] += 1
+    if len(entry_client_receipt.get())!= 0 :
+        j_names.append(entry_client_receipt.get())
+        number['total_receipt_numbers'] += 1
 
-def print_names():
+def print_variables():
     name_count = 0
     Label(main_window, font='bold',text="Name").grid(column=0,row=5)
     ROWS_ABOVE = 3
@@ -24,7 +27,7 @@ def pack_window(): # Fills the window with buttons, labels and text entries
 #   Label(main_window, text="LOLOLOLOL") .grid(column=1,row=0)  #Deprecated test string
     Button(main_window, text="Quit",command=main_window.destroy, width=8, bg='#FF605C', fg='white') .grid(column=0,row=0)
     Button(main_window, text="Append list",command=append_list, width=8, height=6, bg='#FFBD44', fg='white') .grid(column=0,row=1)
-    Button(main_window, text="Print list",command=print_names, width=8, height=2, bg='#61BB46', fg='white') .grid(column=0,row=2)
+    Button(main_window, text="Print list",command=print_variables, width=8, height=2, bg='#61BB46', fg='white') .grid(column=0,row=2)
     Button(main_window, text="Delete row", width=8, height=2, bg='#51A0D5', fg='white') .grid(column=0,row=4)
     Label(main_window, text="Name") .place(x=170,y=0)
     Label(main_window, text="Receipt number") .place(x=118,y=25)
