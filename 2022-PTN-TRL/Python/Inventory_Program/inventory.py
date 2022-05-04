4da76cb04b5fa4059c203b1df2f301d962d12678
Camden Bruce <camden.o.b@gmail.com>
2022-05-05 10:52:33 +1200

    Update file

--- 2022-PTN-TRL/Python/Inventory_Program/inventory.py
+++ 2022-PTN-TRL/Python/Inventory_Program/inventory.py
@@ -1,64 +1,46 @@
 # Camden Bruce
+from cgitb import text
 from tkinter import *
 from tkinter import messagebox
 def main(): # Run the program
     pack_window()
     main_window.mainloop()
 
+def print_variables():
+    name_count = 0
+    while name_count < counters['total_entries'] :
+        Label(main_window, text=name_count).grid(column=10,row=name_count+8)
+        Label(main_window, text=(inventory_details[name_count][0])).grid(column=2,row=name_count+8)
+        Label(main_window, text=(inventory_details[name_count][1])).grid(column=4,row=name_count+8)
+        Label(main_window, text=(inventory_details[name_count][2])).grid(column=6,row=name_count+8)
+        Label(main_window, text=(inventory_details[name_count][3])).grid(column=8,row=name_count+8)
+        name_count += 1
+        counters['name_count'] = name_count
+
 def append_list():
-        j_names.append(entry_client_name.get())
-        number_names['total_names'] += 1
-    
-        j_receipts.append(entry_receipt_number.get())
-        number_receipts['total_receipt_numbers'] += 1
-    
-        j_item_codes.append(entry_item_code.get())
-        number_item_codes['total_item_codes'] += 1
-    
-        j_item_quantity.append(entry_item_quantity.get())
-        number_item_quantity['total_item_quantity'] += 1
-        print("INFO: appending list finished successfully")
+        inventory_details.append([entry_client_name.get(),entry_receipt_number.get(),entry_item_code.get(),entry_item_quantity.get()])
         entry_client_name.delete(0,'end')
         entry_receipt_number.delete(0,'end')
         entry_item_code.delete(0,'end')
         entry_item_quantity.delete(0,'end')
-
-def print_variables():
-    name_count = 0
-    receipt_count = 0
-    item_code_count = 0
-    item_quantity_count = 0
-    ROWS_ABOVE = 3
-    while name_count < number_names['total_names']:
-        Label(main_window, text=(j_names[name_count])).grid(column=0,row=6+name_count + ROWS_ABOVE)
-        name_count += 1
-    while receipt_count < number_receipts['total_receipt_numbers']:
-        Label(main_window, text=(j_receipts[receipt_count])).grid(column=2,row=6+receipt_count + ROWS_ABOVE)
-        receipt_count += 1
-    while item_code_count < number_item_codes['total_item_codes']:
-        Label(main_window, text=(j_item_codes[item_code_count])).grid(column=4,row=6+item_code_count + ROWS_ABOVE)
-        item_code_count += 1
-    while item_quantity_count < number_item_quantity['total_item_quantity']:
-        Label(main_window, text=(j_item_quantity[item_quantity_count])).grid(column=6,row=6+item_quantity_count + ROWS_ABOVE)
-        item_quantity_count += 1
-    print("INFO: printing list finished sucessfully")
-
+        counters['total_entries'] += 1
+        
 def pack_window(): # Fills the window with buttons, labels and text entries
     Button(main_window, text="Quit",command=main_window.destroy, width=8, bg='#FF605C', fg='white') .grid(column=0,row=0)
     Button(main_window, text="Append list",command=error_correction, width=8, height=6, bg='#FFBD44', fg='white') .grid(column=0,row=1)
     Button(main_window, text="Print list",command=print_variables, width=8, height=2, bg='#61BB46', fg='white') .grid(column=0,row=2)
-    Button(main_window, text="Delete row", width=8, height=2, bg='#51A0D5', fg='white') .grid(column=0,row=4)
+    Button(main_window, text="Delete row",command=delete_row, width=8, height=2, bg='#51A0D5', fg='white') .grid(column=0,row=4)
     Label(main_window, text="Name") .place(x=170,y=0)
     Label(main_window, text="Receipt number") .place(x=118,y=25)
     Label(main_window, text="Item code") .place(x=148,y=50)
     Label(main_window, text="Quantity of item").place(x=115,y=75)
     Label(main_window, text="Quantity of item").place(x=115,y=75)
 
-    Label(main_window, font='bold',text="  Name  ").grid(column=0,row=5)
-    Label(main_window, font='bold',text="  Receipt num  ").grid(column=2,row=5)
-    Label(main_window, font='bold',text="  Item code  ").grid(column=4,row=5)
-    Label(main_window, font='bold',text="  Item quantity  ").grid(column=6,row=5)
-    Label(main_window, font='bold',text="  Row  ").grid(column=8,row=5)
+    Label(main_window, font='bold',text="   Name  ").grid(column=2,row=5)
+    Label(main_window, font='bold',text="  Receipt num  ").grid(column=4,row=5)
+    Label(main_window, font='bold',text="  Item code  ").grid(column=6,row=5)
+    Label(main_window, font='bold',text="  Item quantity  ").grid(column=8,row=5)
+    Label(main_window, font='bold',text="  Row").grid(column=10,row=5)
 
 def error_correction():
     missing_inputs = 0
@@ -68,16 +50,15 @@ def error_correction():
     Label(main_window, fg='black', text="Item code") .place(x=148,y=50)
     Label(main_window, fg='black', text="Quantity of item").place(x=115,y=75)
 
-    if str(entry_receipt_number.get()).isdigit() == False: #and len(entry_receipt_number.get()) !=0:
+    if str(entry_receipt_number.get()).isdigit() == False:
         non_int_inputs = 1
         Label(main_window, fg='green', text="Receipt number") .place(x=118,y=25)
-    if str(entry_item_code.get()).isdigit() == False: # and len(entry_item_code.get()) !=0:
+    if str(entry_item_code.get()).isdigit() == False:
         non_int_inputs = 1
         Label(main_window, fg='green', text="Item code") .place(x=148,y=50)
-    if str(entry_item_quantity.get()).isdigit() == False: # and len(entry_item_quantity.get()) !=0:
+    if str(entry_item_quantity.get()).isdigit() == False:
         non_int_inputs = 1
         Label(main_window, fg='green', text="Quantity of item").place(x=115,y=75)
-
     if len(entry_client_name.get()) == 0:
         missing_inputs = 1
         Label(main_window, fg='red', text="Name") .place(x=170,y=0)
@@ -96,15 +77,19 @@ def error_correction():
         messagebox.showerror('Inventory program error', 'Error: non integer input. Errors highlighted in green')
     else:
         append_list()
-#def delete_row():
-    
-number_names = {'total_names':0}
-number_receipts = {'total_receipt_numbers':0}  
-number_item_codes = {'total_item_codes':0}
-number_item_quantity = {'total_item_quantity':0}
+def delete_row():
+    del inventory_details[int(delete_row.get())]
+    counters['total_entries'] -= 1
+    name_count = counters['name_count']
+    delete_row.delete(0,'end')
+    Label(main_window, text="        ").grid(column=2,row=name_count+7) 
+    Label(main_window, text="        ").grid(column=4,row=name_count+7)
+    Label(main_window, text="        ").grid(column=6,row=name_count+7)
+    Label(main_window, text="        ").grid(column=8,row=name_count+7)
+    Label(main_window, text="        ").grid(column=10,row=name_count+7)
 
-plu_numbers = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
-plu_conversions = ["Spoons", "Forks", "Plates", "Knifes", "Teaspoons", "Pots", "Pans", "Bowls", "Cups", "Mugs"]
+counters = {'total_entries':0,'name_count':0}
+inventory_details = []
 j_names = []
 j_receipts = []
 j_item_codes = []
@@ -112,8 +97,8 @@ j_item_quantity = []
 
 main_window = Tk()
 main_window.title('Stock inventory')
-main_window.geometry('600x300')
-main_window.resizable(False, True)
+#main_window.geometry('600x300')
+#main_window.resizable(False, True)
 entry_client_name = Entry(main_window, width=38)
 entry_client_name.place(x=220,y=0)
 entry_receipt_number = Entry(main_window, width=38)
